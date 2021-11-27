f50ccd97e9325987e396c4a4dfea7e66c6116c37
Riswan Gani Padilah <riswangani11@gmail.com>
2021-11-27 07:15:39 +0700

    tampilan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,8 +27,9 @@
 
 @auth
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <button type="button" class="btn mb-3 btn-primary btn-lg">Add Postingan</button>
+    
+        {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -42,8 +43,24 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
-        </div>
-    </div>
+        </div> --}}
+
+          <div class="row">
+            <div class="col-sm-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">Riswan Gani</p>
+                  <p class="card-text">Jawa Barat</p>
+                  <p class="card-text">Bandung</p>
+                  <a href="#" class="btn btn-block text-center btn-primary">Detail</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+    
 </div>
 @endauth
 @endsection
