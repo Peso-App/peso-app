56e04c555ce9bda6fb2f24e33642e209250b1d7b
Riswan Gani Padilah <riswangani11@gmail.com>
2021-12-24 20:19:17 +0700

    add meta dicoding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="dicoding:email" content="riswangani11@gmail.com">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
